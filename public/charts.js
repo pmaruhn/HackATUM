8ff6bccda226048df9a4660cad52ad4c2b86d94e
Lorenz Prasch <lorenz.prasch@gmail.com>
2020-04-18 17:24:33 +0200

    background chart styling

--- public/charts.js
+++ public/charts.js
@@ -9,34 +9,43 @@ var EmotionBarChartObject = new Chart(ctxEmotionBarChart, {
         label: '# of Votes',
         data: [],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
           'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
           'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
+        ]
       },
     ],
   },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
+  options: { 
+    legend: {
+      labels: {
+        fontColor: "white",
+        fontSize: 18,
+        fontFamily: "'Alegreya Sans', sans-serif"
+      }
     },
-  },
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: "white",
+          fontSize: 18,
+          stepSize: 1,
+          fontFamily: "'Alegreya Sans', sans-serif",
+          beginAtZero: true
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          fontColor: "white",
+          fontSize: 14,
+          stepSize: 1,
+          fontFamily: "'Alegreya Sans', sans-serif",
+          beginAtZero: true
+        }
+      }]
+    }
+  }
 });
