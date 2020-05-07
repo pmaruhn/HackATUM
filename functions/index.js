7551b29148b2650c85263e2aa5b395f60c0458b4
pmaruhn <philipp.maruhn@posteo.de>
2020-05-07 10:55:26 +0200

    tts request

--- functions/index.js
+++ functions/index.js
@@ -25,34 +25,35 @@ exports.learnerState = functions.https.onRequest(async (req, res) => {
   res.send('Data pushed to database');
 });
 
-const textToSpeech = require('@google-cloud/text-to-speech');
+exports.ttsreq = functions.https.onRequest((req, res) => {
+  const textToSpeech = require('@google-cloud/text-to-speech');
 
-// Import other required libraries
-const fs = require('fs');
-const util = require('util');
-// Creates a client
-const client = new textToSpeech.TextToSpeechClient();
+  // Import other required libraries
+  const fs = require('fs');
+  const util = require('util');
+  // Creates a client
+  const client = new textToSpeech.TextToSpeechClient();
+  async function quickStart() {
+    // The text to synthesize
+    const text = 'Was für eine phenomenale Vorlesung!';
 
-exports.tts = functions.https.onCall((data, context) => {
-  // function quickStart(data) {
-  // The text to synthesize
-  const text = data.question;
-  return data.question;
-  // Construct the request
-  // const googlerequest = {
-  //   input: { text: text },
-  //   // Select the language and SSML voice gender (optional)
-  //   voice: { languageCode: 'de-DE', ssmlGender: 'MALE' },
-  //   // select the type of audio encoding
-  //   audioConfig: { audioEncoding: 'MP3' },
-  // };
-  // // Performs the text-to-speech googlerequest
-  // const [googleresponse] = await client.synthesizeSpeech(googlerequest);
-  // // Write the binary audio content to a local file
-  // // const writeFile = util.promisify(fs.writeFile);
-  // // await writeFile('output.mp3', googleresponse.audioContent, 'binary');
-  // // return googleresponse.audioContent;
+    // Construct the request
+    const request = {
+      input: { text: text },
+      // Select the language and SSML voice gender (optional)
+      voice: { languageCode: 'de-DE', ssmlGender: 'MALE' },
+      // select the type of audio encoding
+      audioConfig: { audioEncoding: 'MP3' },
+    };
 
-  // // }
-  // // quickStart(data);
+    // Performs the text-to-speech request
+    const [response] = await client.synthesizeSpeech(request);
+    // Write the binary audio content to a local file
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile('test.mp3', response.audioContent, 'binary');
+    console.log('Audio content written to file: output.mp3');
+    res.send('Audio content written to file: test.mp3');
+  }
+  quickStart();
+  // [END tts_quickstart]
 });
